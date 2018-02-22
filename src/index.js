99384945203928b2688d52b99a57b87c56a43af1
Oleg <crashyourselfcontrol@gmail.com>
2018-02-23 02:19:04 +0300

    code monkey

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = getLoveTrianglesCount;
-
-console.log('getLoveTrianglesCount([2,3,1]): ', getLoveTrianglesCount([2, 3, 1]));
-
-
-function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
-
-  preferences.forEach((item, i, arr) => {
-    if (i + 1 == arr[arr[arr[i] - 2]]) {
-      count = count + 1;
-    }
+  let arr = preferences.map(item => {
+    return item - 1;
   });
 
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] != null && (i == arr[arr[arr[i]]])) {
+      arr[i] = null;
+      arr[arr[i]] = null;
+      arr[arr[arr[i]]] = null;
+      count++;
+    }
+  }
   return count;
 };
