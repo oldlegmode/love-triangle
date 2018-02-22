abee850972859f0efcd4c82de1f0de7890f89980
Oleg <crashyourselfcontrol@gmail.com>
2018-02-23 02:47:49 +0300

    much beter style code

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
-  let arr = preferences.map(item => {
-    return item - 1;
-  });
-
-  arr.forEach((item, i, arr) => {
-    if (arr[i] !== arr[arr[arr[i]]] && (i == arr[arr[arr[i]]])) {
-      arr[i] = null;
-      arr[arr[i]] = null;
-      arr[arr[arr[i]]] = null;
-      count++;
-    }
-  });
+  preferences
+    .map(item => item - 1)
+    .forEach((item, i, arr) => {
+      if (arr[i] !== arr[arr[arr[i]]] && i == arr[arr[arr[i]]]) {
+        arr[i] = null;
+        arr[arr[i]] = null;
+        arr[arr[arr[i]]] = null;
+        count++;
+      }
+    });
   return count;
 };
