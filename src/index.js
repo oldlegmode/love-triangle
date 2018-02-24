9b42cbeb727b8b45323ff15f01cd67ff25f6bce7
Oleg <crashyourselfcontrol@gmail.com>
2018-02-24 18:52:35 +0300

    solution with reduce

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  let count = 0;
-  preferences
+  return preferences
     .map(item => item - 1)
-    .forEach((item, i, arr) => {
+    .reduce((count, item, i, arr) => {
       if (arr[i] !== arr[arr[arr[i]]] && i == arr[arr[arr[i]]]) {
         arr[i] = null;
         arr[arr[i]] = null;
         arr[arr[arr[i]]] = null;
         count++;
       }
-    });
-  return count;
+      return count;
+    }, 0);
 };
